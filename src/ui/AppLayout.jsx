b1ea3f0f495d6afc9d2416307857e5565ede2aee
com1lc <plato2h@gmail.com>
2024-01-18 20:43:47 +0700

    initial

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,25 @@
+import Header from './Header';
+import CartOverView from '../features/cart/CartOverview';
+import { Outlet, useNavigate, useNavigation } from 'react-router-dom';
+import Loader from './Loader';
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
+  return (
+    <div className="grid  h-screen grid-rows-[auto_1fr_auto] ">
+      {isLoading && <Loader />}
+
+      <Header />
+      <div className=" overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+      <CartOverView />
+    </div>
+  );
+}
+
+export default AppLayout;
